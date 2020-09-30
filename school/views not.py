39bd4a6a0330448b8009f734bb6469f46e4b8f50
BKVDPrasad <BKVDPrasad@users.noreply.github.com>
2020-09-30 23:09:12 +0530

    add

---
+++ school/views not.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import CreateAPIView,ListAPIView
+from .serializer import PAS_S40_Institution,PAS_S40_InstitutionSerializer
+class PAS_40_CreateSchool(CreateAPIView):
+    queryset = PAS_S40_Institution
+    serializer_class = PAS_S40_InstitutionSerializer
+
+
+class PAS_40_ViewSchool(ModelViewSet):
+    queryset = PAS_S40_Institution.objects.all()
+    serializer_class = PAS_S40_InstitutionSerializer
